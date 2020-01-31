353273ce11d7537e525dd7c94e7bedb1d8528217
Shizhao Yang <nicolasysz97@gmail.com>
2020-01-31 02:11:21 -0500

    HW2

--- assignment2_ws/src/assignment2_turtlesim/src/square_openloop.py
+++ assignment2_ws/src/assignment2_turtlesim/src/square_openloop.py
@@ -4,43 +4,48 @@ from geometry_msgs.msg import Twist
 
 def move():
     #Starts a new node
+    PI = 3.14159265389
     rospy.init_node('robot',anonymous=True)
     velocity_publisher =rospy.Publisher('turtle1/cmd_vel',Twist,queue_size=10)
     vel_msg = Twist()
-
     #Receiving the user's input
     print("Lets move your robot")
-    speed = input("Input your speed:")
     distance = input("Type your distance:")
-    isForward = input("Forward?: ")
     #Checking if the movement is forward or backwards
-    if (isForward):
-         vel_msg.linear.x =abs(speed)
-    else: 
-         vel_msg.linear.x = -abs(speed)
     #Since we are moving just in x-axis
     vel_msg.linear.y = 0
     vel_msg.linear.z = 0
     vel_msg.angular.x = 0
     vel_msg.angular.y = 0
-    vel_msg.angular.z = 0.2
-    while not rospy.is_shutdown():
+    t0 = rospy.Time.now().to_sec()
+    current_distance = 0
+    count = 0
+    while (1):
         #Setting the current time for distance calculus
-        t0 =rospy.Time.now().to_sec()
-        current_distance = 0
-
+        vel_msg.linear.x = 0.2
+        vel_msg.angular.z = 0
+        velocity_publisher.publish(vel_msg)
+        t1 = rospy.Time.now().to_sec()
+        current_distance=(t1-t0)*vel_msg.linear.x
         #Loop to move the turtle in an specified distance
-        while (current_distance < distance):
+        count = count + 1
+        if current_distance > distance :
+            t2 = rospy.Time.now().to_sec()
+        while (current_distance > distance):
+            vel_msg.linear.x = 0
+            vel_msg.angular.z = 0.2
             #Publish the velocity
             velocity_publisher.publish(vel_msg)
             #Takes actual time to velocity calculus
-            t1 = rospy.Time.now().to_sec()
-            #Calculates distancePoseStamped
-            current_distance = vel_msg.angular.z*(t1-t0)
+            t3 = rospy.Time.now().to_sec()
+            current_angular = (t3-t2)* 0.2
+            if current_angular > PI/2:
+                current_distance = 0
+                t0 = rospy.Time.now().to_sec()
         #After the loop,stops the robot
-        vel_msg.linear.x = 0
+        #vel_msg.linear.x = 0
         #Force the robot to stop
-        velocity_publisher.publish(vel_msg)
+        #velocity_publisher.publish(vel_msg)
 if __name__ == '__main__':
     try:
         #Testing our function
